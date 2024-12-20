843a76609aa9a029fbd0dc412f93dbf3233342ce
manueldembo <madembo18@gmail.com>
2024-12-20 13:30:08 +0100

    feat: list orders using graphql

--- internal/infra/graph/resolver.go
+++ internal/infra/graph/resolver.go
@@ -8,4 +8,5 @@ import "github.com/manueldembo/clean_architecture/internal/usecase"
 
 type Resolver struct {
 	CreateOrderUseCase usecase.CreateOrderUseCase
+	ListOrdersUseCase  usecase.ListOrdersUseCase
 }
